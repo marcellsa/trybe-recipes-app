8771032fffeeee370cdfd1b4e9b7951892d95182
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-10-26 16:28:15 -0300

    Req 16 - testes desenhados e renderWithRouter construído.

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -5,11 +5,27 @@ import App from '../App';
 // importar páginas
 
 describe('Testando o compoenente Footer', () => {
-  test('Se o botão de meals está presente na página Meals', () => {
+  test('1) Se o ícone de meals está presente na página Meals', () => {
+    //renderwithrouter
+    const nealsButton = screen.getByTestId("meals-bottom-btn");
+    expect(nealsButton).toBeInTheDocument();
+  });
 
-  })
+  test('2) Se o ícone de drinks está presente na página Meals', () => {
+    //renderwithrouter
+    const drinksButton = screen.getByTestId("drinks-bottom-btn");
+    expect(drinksButton).toBeInTheDocument();
+  });
 
-  test('Se o botão de drinks está presente na página Meals', () => {
+  test('3) Se o ícone de meals está presente na página Drinks', () => {
+    //renderwithrouter
+    const nealsButton = screen.getByTestId("meals-bottom-btn");
+    expect(nealsButton).toBeInTheDocument();
+  });
 
-  })
+  test('4) Se o ícone de drinks está presente na página Drinks', () => {
+    //renderwithrouter
+    const drinksButton = screen.getByTestId("drinks-bottom-btn");
+    expect(drinksButton).toBeInTheDocument();
+  });
 })
