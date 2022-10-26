31a759b0abc7808a40d7dc8a3e65edd4cd77bf26
Arthutepio <arthurodriguesr10@gmail.com>
2022-10-26 16:11:42 -0300

    correção

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,44 +1,8 @@
-import React, { useContext, useEffect } from 'react';
-// import Header from '';
-// import Footer from '';
-import Recipes from '../components/Recipes';
-import Context from '../context/Context';
+import React from 'react';
 
-function Drinks() {
-  const cinco = 5;
-
-  const { setMealOrDrink, setReturnAPI, setCategory } = useContext(Context);
-
-  const getDrinkAPI = async () => { // requisição à API com o endpoint como parâmetro, pois será decidido apenas após aperta o botão de busca
-    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
-    const json = await response.json();
-    setReturnAPI(json);
-  };
-
-  const getCategories = async () => { // requisição à API com o endpoint como parâmetro, pois será decidido apenas após aperta o botão de busca
-    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
-    const json = await response.json();
-    const fiveCategories = json.drinks.slice(0, cinco);
-    setCategory(fiveCategories);
-  };
-
-  useEffect(() => {
-    setMealOrDrink('drink');
-    getCategories();
-    getDrinkAPI();
-  }, []);
-
+export default function Drinks() {
   return (
-    <div>
-      <Header page="Drinks" />
-      <Recipes />
-      <Footer />
-    </div>
+    <div>Drinks</div>
   );
 }
 
-export default Drinks;
-
-/*
-Se for um único drink, renderiza o header, o drink e o footer
-*/
