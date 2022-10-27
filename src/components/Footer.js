9db45024b96b8bf38310d8ae4225e8cabf1ecd26
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-10-27 18:00:12 -0300

    Req 16 - altertarando caminho das ícones.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
   return (
-    <footer data-testid="footer" >      
+    <footer data-testid="footer">
       Footer
-      <button>
-        <img data-testid="drinks-bottom-btn" src="src/images/drinkIcon.svg" alt="ícone de bebidas"/>
+      <button type="button">
+        <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="ícone de bebidas" />
       </button>
-      
-      <button>
-        <img data-testid="meals-bottom-btn" src="src/images/mealIcon.svg" alt="ícone de comidas"/>
+
+      <button type="button">
+        <img data-testid="meals-bottom-btn" src={ mealIcon } alt="ícone de comidas" />
       </button>
     </footer>
-  )
+  );
 }
 
 export default Footer;
