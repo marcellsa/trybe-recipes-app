22ab05870a9b7e8a405aeee98cae15150fedeed3
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-10-28 14:53:23 -0300

    Lógica criada para redirecionar o usuário para tela correta - sem testes.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
@@ -6,13 +7,18 @@ function Footer() {
   return (
     <footer data-testid="footer">
       Footer
-      <button type="button">
-        <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="ícone de bebidas" />
-      </button>
+      <Link to='/drinks'>
+        <button type="button">
+          <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="ícone de bebidas" />
+        </button>
+      </Link>
 
-      <button type="button">
-        <img data-testid="meals-bottom-btn" src={ mealIcon } alt="ícone de comidas" />
-      </button>
+      <Link to="/meals">
+        <button type="button">
+          <img data-testid="meals-bottom-btn" src={ mealIcon } alt="ícone de comidas" />
+        </button>
+      </Link>
+
     </footer>
   );
 }
