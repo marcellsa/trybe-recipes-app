1323f9c9eec6682640ad05487fe8988cc10d4e29
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-10-28 15:49:04 -0300

    Req 16 - ajustando erros no testes - 100 porcento testado.

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,51 +1,33 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
-import { act } from 'react-dom/test-utils';
-// import Meals from '../pages/Meals';
-// import Drinks from '../pages/Drinks';
-
-// importar páginas
+import Meals from '../pages/Meals';
+import Drinks from '../pages/Drinks';
 
 describe('Testando o compoenente Footer', () => {
   test('1) Se o ícone de meals está presente na página Meals', () => {
-   
-    const { history } = renderWithRouter(<App />);
-    act(() => {
-      history.push('/meals');
-    })
+    renderWithRouter(<Meals />);
     const mealsButton = screen.getByTestId('meals-bottom-btn');
     expect(mealsButton).toBeInTheDocument();
   });
 
   test('2) Se o ícone de drinks está presente na página Meals', () => {
-    
-    const { history } = renderWithRouter(<App />);
-    act(() => {
-      history.push('/meals');
-    })
+    renderWithRouter(<Meals />);
     const drinksButton = screen.getByTestId('drinks-bottom-btn');
     expect(drinksButton).toBeInTheDocument();
   });
 
   test('3) Se o ícone de meals está presente na página Drinks', () => {
-
-    const { history } = renderWithRouter(<App />);
-    act(() => {
-      history.push('/drinks');
-    })
+    renderWithRouter(<Drinks />);
     const mealsButton = screen.getByTestId('meals-bottom-btn');
     expect(mealsButton).toBeInTheDocument();
   });
 
   test('4) Se o ícone de drinks está presente na página Drinks', () => {
-    
-    const { history } = renderWithRouter(<App />);
-    act(() => {
-      history.push('/drinks');
-    })
+    renderWithRouter(<Drinks />);
     const drinksButton = screen.getByTestId('drinks-bottom-btn');
     expect(drinksButton).toBeInTheDocument();
   });
-})
+});
