f2ec83bb30fe5f19288b05f383874bf7bb48ce1b
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-11-04 17:46:29 -0300

    Lógica do Req 44 desenvolvida.

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,59 +1,63 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import shareIcon from '../images/shareIcon.svg';
 
-const MOCK_TEST = [
+const DONE_RECIPES_KEY_TEST = [
   {
-    idMeal: '52977',
-    strArea: 'Turkish',
-    strCategory: 'Side',
-    strMeal: 'Corba',
-    strMealThumb: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
-    strTags: 'Soup',
+    id: '52771',
+    type: 'meal',
+    nationality: 'Italian',
+    category: 'Vegetarian',
+    alcoholicOrNot: '',
+    name: 'Spicy Arrabiata Penne',
+    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+    doneDate: '23/06/2020',
+    tags: ['Pasta', 'Curry'],
   },
   {
-    idMeal: '53026',
-    strArea: 'Egyptian',
-    strCategory: 'Vegetarian',
-    strMeal: 'Tamiya',
-    strMealThumb: 'https://www.themealdb.com/images/media/meals/n3xxd91598732796.jpg',
-    strTags: null,
-  },
-  {
-    idDrink: '15997',
-    strCategory: 'Ordinary Drink',
-    strDrink: 'GG',
-    strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
-    strTags: null,
+    id: '178319',
+    type: 'drink',
+    nationality: '',
+    category: 'Cocktail',
+    alcoholicOrNot: 'Alcoholic',
+    name: 'Aquamarine',
+    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+    doneDate: '23/06/2020',
+    tags: [],
   },
 ];
 
+// localStorage.setItem será usado apenas para desenvolvimento e teste. Será retirado depois
+localStorage.setItem('doneRecipes', JSON.stringify(DONE_RECIPES_KEY_TEST));
+
 export default function DoneRecipes() {
   const [filterButton, setFilterButton] = useState('All');
-  const [results, setResults] = usaState([]);
+  const [localStorageData, setLocalStorageData] = usaState([]);
 
   const getDoneRecipes = () => {
-    localStorage.setItem('doneRecipes', JSON.stringify(MOCK_TEST));
-    const doneRecipesKey = localStorage.getItem('doneRecipes');
-    return JSON.parse(doneRecipesKey);
+    const doneRecipesInfo = localStorage.getItem('doneRecipes');
+    return JSON.parse(doneRecipesInfo);
   };
 
+  const getData = useCallback(() => {
+    const data = getDoneRecipes();
+    setLocalStorageData(data);
+  }, [setLocalStorageData]);
+
   const handleFilterButtonChange = useCallback(() => {
-    const localStorageData = getDoneRecipes();
-    if (filterButton === 'Meals') {
-      const newResult = localStorage.filter((recipe) => recipe.includes(recipe.idMeal));
-      setResults(newResult);
-    } else if (filterButton === 'Drinks') {
-      const newResult = localStorage.filter((recipe) => recipe.includes(recipe.idDrink));
-      setResults(newResult);
+    const newData = getDoneRecipes();
+    if (filterButton !== 'All') {
+      const results = newData.filter(({ type }) => type === filterButton);
+      setLocalStorageData(results);
     } else {
-      setResults(localStorageData);
+      setLocalStorageData(newData);
     }
-  }, [filterButton, setResults]);
+  }, [filterButton, setLocalStorageData]);
 
   useEffect(() => {
-    getDoneRecipes();
-  }, []);
+    getData();
+  }, [getData]);
 
   useEffect(() => {
     handleFilterButtonChange();
@@ -71,78 +75,52 @@ export default function DoneRecipes() {
       >
         All
       </button>
+
       <button
         data-testid="filter-by-meal-btn"
         type="button"
-        onClick={ () => setFilterButton('Meals') }
+        onClick={ () => setFilterButton('meal') }
       >
         Meals
       </button>
+
       <button
         data-testid="filter-by-drink-btn"
         type="button"
-        onClick={ () => setFilterButton('Drinks') }
+        onClick={ () => setFilterButton('drink') }
       >
         Drinks
       </button>
 
-      {results?.map((recipe, index) => (
-        <div key={ recipe.id }>
-          <Link
-            to={
-              recipe.includes(recipe.idMeal)
-                ? `/meals/${recipe.idMeal}`
-                : `/drinks/${recipe.idDrink}`
-            }
-          >
-            <img
-              src={ recipe.image }
-              data-testid={ `${index}-horizontal-image` }
-              alt="recipe"
-              width="250px"
-            />
-          </Link>
-          <p data-testid={ `${index}-horizontal-top-text` }>
-            {recipe.type === 'meal'
-              ? `${recipe.nationality} - ${recipe.category}`
-              : `${recipe.alcoholicOrNot}`}
-          </p>
-          <Link
-            to={
-              recipe.type === 'meal'
-                ? `/meals/${recipe.id}`
-                : `/drinks/${recipe.id}`
-            }
-          >
-            <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
-          </Link>
-
+      {localStorageData.length !== 0 && localStorageData.map((recipe, index) => (
+        <div key={ index }>
+          <img
+            data-testid={ `${index}-horizontal-image` }
+            src={ recipe.image }
+            alt="imagem da receita"
+            width="250px"
+          />
+          <p data-testid={ `${index}-horizontal-top-text` }>{recipe.category}</p>
+          <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
           <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
-
-          <button
-            type="button"
-            onClick={ () => getURL(recipe) }
-            className="btnURL"
-          >
-            <img
-              src={ shareIcon }
-              alt="share"
-              data-testid={ `${index}-horizontal-share-btn` }
-              className="doneRecipesShare"
-            />
-          </button>
+          <img
+            data-testid={ `${index}-horizontal-share-btn` }
+            src={ shareIcon }
+            alt="ìcone de compartilhamento"
+          />
           <ul>
-            {recipe?.tags.map((tagName) => (
+            {recipe?.tags.map((tag) => (
               <li
-                data-testid={ `${index}-${tagName}-horizontal-tag` }
-                key={ tagName }
+                key={ tag }
+                data-testid={ `${index}-${tag}-horizontal-tag` }
               >
-                {tagName}
+                {tag}
               </li>
             ))}
           </ul>
         </div>
       ))}
+
     </div>
   );
 }
