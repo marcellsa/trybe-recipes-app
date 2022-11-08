7518fa7ad0d409543bb63bf700b05198dc0dd48b
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-11-08 10:46:20 -0300

    feat: Req46 - adicionando 2 testes.

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+// import userEvent from '@testing-library/user-event';
 import DoneRecipes from '../pages/DoneRecipes';
 import renderWithRouter from './renderWithRouter';
 
@@ -13,6 +13,8 @@ const DONE_RECIPES_KEY_MOCK_TEST = [
     alcoholicOrNot: '',
     name: 'Spicy Arrabiata Penne',
     image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+    doneDate: '23/06/2020',
+    tags: ['Pasta', 'Curry'],
   },
   {
     id: '178319',
@@ -22,10 +24,31 @@ const DONE_RECIPES_KEY_MOCK_TEST = [
     alcoholicOrNot: 'Alcoholic',
     name: 'Aquamarine',
     image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+    doneDate: '23/06/2020',
+    tags: [],
   },
 ];
+
 describe('Testing DoneRecipes page', () => {
   beforeEach(() => {
-    window.localStorage.clear();
+    localStorage.clear();
+    localStorage.setItem('doneRecipes', JSON.stringify(DONE_RECIPES_KEY_MOCK_TEST));
+  });
+
+  test('Verifica se os botões de categorias existem ', () => {
+    renderWithRouter(<DoneRecipes />);
+
+    const allFilterButton = screen.getByTestId('filter-by-all-btn');
+    const mealFilterButton = screen.getByTestId('filter-by-meal-btn');
+    const drinkFilterButton = screen.getByTestId('filter-by-drink-btn');
+
+    expect(allFilterButton && drinkFilterButton && mealFilterButton).toBeInTheDocument();
+  });
+
+  test('Verifica se existem 2 receitas aparecendo na tela', async () => {
+    renderWithRouter(<DoneRecipes />);
+    const firstRecipe = await screen.findByText(/spicy arrabiata penne/i);
+    const secondRecipe = await screen.findByText(/aquamarine/i);
+    expect(firstRecipe && secondRecipe).toBeInTheDocument();
   });
 });
