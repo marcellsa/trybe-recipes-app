24c521fa64d2c52871ff3b0a33184998548116de
PedrovRamos <pvramos.cunha@gmail.com>
2022-11-09 17:25:03 -0300

    Adicionando projeto com testes de searchbar e recipes passando com 45%

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import Context from '../context/Context';
 import SearchBar from '../components/SearchBar';
+import App from '../App';
 
 describe('Testa o componente SearchBar', () => {
   const INITIAL_STATE = {
@@ -26,5 +27,36 @@ describe('Testa o componente SearchBar', () => {
     expect(radioBtnFirstLetter).toBeInTheDocument();
     expect(radioBtnName).toBeInTheDocument();
     expect(radioBtnIngredients).toBeInTheDocument();
+  });
+
+  it('testa se a pesquisa é feita corretamente', async () => {
+    const { history } = renderWithRouter(<App />);
+    history.push = jest.fn();
+    const emailInput = screen.getByRole('textbox');
+    const passInput = screen.getByTestId('password-input');
+    const btnSubmit = screen.getByRole('button', { name: /enter/i });
+    userEvent.type(emailInput, 'vasconaseriaa@gmail.com');
+    userEvent.type(passInput, '12345678');
+    userEvent.click(btnSubmit);
+
+    const searchBtn = screen.getByRole('img', { name: /search/i });
+    userEvent.click(searchBtn);
+
+    const inputSearch = screen.getByRole('textbox');
+    userEvent.type(inputSearch, 'potato');
+    expect(inputSearch).toBeInTheDocument();
+
+    const radioBtnIngredients = screen.getByTestId('ingredient-search-radio');
+    userEvent.click(radioBtnIngredients);
+
+    const execSearchBtn = screen.getByTestId('exec-search-btn');
+    expect(execSearchBtn).toBeInTheDocument();
+
+    userEvent.click(execSearchBtn);
+
+    await waitFor(() => {
+      expect(history.push).toHaveBeenCalledWith('/meals/52782');
+      expect(history.push).toHaveBeenCalledTimes(1);
+    });
   });
 });
