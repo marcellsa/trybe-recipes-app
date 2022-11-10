b26d80afe29d85df4ea7587130ebf283fbcf3c46
PedrovRamos <pvramos.cunha@gmail.com>
2022-11-10 16:40:12 -0300

    Adicionando projeto com req7 passando

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -3,34 +3,6 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import shareIcon from '../images/shareIcon.svg';
 
-// const DONE_RECIPES_KEY_TEST = [
-//   {
-//     id: '52771',
-//     type: 'meal',
-//     nationality: 'Italian',
-//     category: 'Vegetarian',
-//     alcoholicOrNot: '',
-//     name: 'Spicy Arrabiata Penne',
-//     image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-//     doneDate: '23/06/2020',
-//     tags: ['Pasta', 'Curry'],
-//   },
-//   {
-//     id: '178319',
-//     type: 'drink',
-//     nationality: '',
-//     category: 'Cocktail',
-//     alcoholicOrNot: 'Alcoholic',
-//     name: 'Aquamarine',
-//     image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-//     doneDate: '23/06/2020',
-//     tags: [],
-//   },
-// ];
-
-// // localStorage.setItem será usado apenas para desenvolvimento e teste. Será retirado depois
-// localStorage.setItem('doneRecipes', JSON.stringify(DONE_RECIPES_KEY_TEST));
-
 export default function DoneRecipes() {
   const [filterButton, setFilterButton] = useState('All');
   const [localStorageData, setLocalStorageData] = useState([]);
@@ -100,7 +72,9 @@ export default function DoneRecipes() {
         Drinks
       </button>
 
-      {localStorageData.length !== 0 && localStorageData.map((recipe, index) => (
+      {/* {console.log(local)} */}
+
+      {localStorageData !== null && localStorageData.map((recipe, index) => (
         <div key={ index }>
           <Link
             to={ recipe.type === 'meal'
